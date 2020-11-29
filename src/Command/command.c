28061a66f83abc11aa3d9031ad30538d4c970a2f
imulki <imamnurulhukmi@gmail.com>
2020-11-30 02:59:01 +0700

    nambah komentar untuk command

--- src/Command/command.c
+++ src/Command/command.c
@@ -3,6 +3,8 @@
 #include "command.h"
 
 void Buy(Player * P, TabBarang B, IdxTypeBarang i, int Jumlah) {
+/* I.S. P terdefinisi, B adalah tabel daftar barang, i adalah idx barang yang valid, Jumlah adalah jumlah barang */
+/* F.S. Jumlah barang dengan idx i pada P bertambah sebanyak Jumlah, uang pada P berkurang sesuai ketentuan harga barang */
     ElTypeBarang Barang;
     Barang = ElmtBarang(B, i-1);
     int HargaTotal;
@@ -18,6 +20,8 @@ void Buy(Player * P, TabBarang B, IdxTypeBarang i, int Jumlah) {
 
 // Membuat wahana baru
 void BuildWahana(Wahana W, Player * P, MATRIKS * M, POINT LokasiBuild) {
+/* I.S. P terdefinisi, W adalah wahana yang ingin dibangun, M adalah map taman bermain, lokBuild adalah koordinat tempat wahana akan dibangun */
+/* F.S. wahana terbangun pada lokasi lokBuild di M, uang serta material P berkurang */
     if (IsEnough(*P, W.biayaBuild)) {
 
         boolean IsMaterialEnough;
@@ -36,7 +40,12 @@ void BuildWahana(Wahana W, Player * P, MATRIKS * M, POINT LokasiBuild) {
 
 // Mengupgrade wahana (Prekondisi : Player sudah berada di sebelah wahana.)
 void UpgradeWahana(BinTree T, Player * P, MATRIKS * M, int i, POINT LokasiWahana, List * L, int z) {
-
+/* I.S. T adalah BinTree Upgrade wahana, P terdefinisi, M adalah map, L adalah List wahana yang telah dibangun */
+/* lokUpgrade adalah koordinat wahana yang ingin diupgrade, zona adalah zona map wahana */
+/* i adalah arah upgrade: 
+    1. subpohon kiri, 
+    2. subpohon kanan,  */
+/* F.S. wahana pada L yang berkoordinat di lokUpgrade dengan zona zona terupgrade, material dan uang pada P berkurang, tanda wahana di M berubah */
     if (i == 1) {   /* Memilih upgrade 1 (Left) */
         if (IsEnough(*P, Akar(Left(T)).biayaBuild)) {
 
--- src/Command/command.h
+++ src/Command/command.h
@@ -11,11 +11,24 @@
 
 // Buy material
 void Buy(Player * P, TabBarang B, IdxTypeBarang i, int Jumlah);
+/* I.S. P terdefinisi, B adalah tabel daftar barang, i adalah idx barang yang valid, Jumlah adalah jumlah barang */
+/* F.S. Jumlah barang dengan idx i pada P bertambah sebanyak Jumlah, uang pada P berkurang sesuai ketentuan harga barang */
+
 
 // Membuat wahana baru
 void BuildWahana(Wahana W, Player * P, MATRIKS * M, POINT lokBuild);
+/* I.S. P terdefinisi, W adalah wahana yang ingin dibangun, M adalah map taman bermain, lokBuild adalah koordinat tempat wahana akan dibangun */
+/* F.S. wahana terbangun pada lokasi lokBuild di M, uang serta material P berkurang */
+
 
 // Mengupgrade wahana
 void UpgradeWahana(BinTree T, Player * P, MATRIKS * M, int i, POINT lokUpgrade, List * L, int zona);
+/* I.S. T adalah BinTree Upgrade wahana, P terdefinisi, M adalah map, L adalah List wahana yang telah dibangun */
+/* lokUpgrade adalah koordinat wahana yang ingin diupgrade, zona adalah zona map wahana */
+/* i adalah arah upgrade: 
+    1. subpohon kiri, 
+    2. subpohon kanan,  */
+/* F.S. wahana pada L yang berkoordinat di lokUpgrade dengan zona zona terupgrade, material dan uang pada P berkurang, tanda wahana di M berubah */
+
 
 #endif
